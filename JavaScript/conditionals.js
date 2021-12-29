a328c27ecada49e2c6ee468eeaf1df15ef95be94
SubhanRaj <shubhanraj2002@gmail.com>
2021-12-29 10:46:36 +0530

    Conditionals in JS

---
+++ JavaScript/conditionals.js
@@ -0,0 +1,14 @@
+let a = 5;
+
+if (a >= 5) {
+    for (a = 0; a <= 50; a++) {
+        if (a <= 30) {
+            console.log("The value of a is", a);
+        }
+
+    }
+} else {
+    console.log("The value of a is", a);
+}
+
+// if-else & else if is similar to C & C++ in JavaScript
