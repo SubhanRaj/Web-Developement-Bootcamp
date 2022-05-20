403ec720d6b7f0998f842130a8048bfecf0c8b3b
Subhan Raj <shubhanraj2002@gmail.com>
2022-05-20 09:47:03 +0530

    JS Variables

---
+++ JavaScript/variables.js
@@ -0,0 +1,37 @@
+// All variables in JS
+// We use var for variables
+
+var num1 = 24324;
+var num2 = 15.652;
+
+console.log(num1);
+console.log(num2);
+console.log (num1 + num2);
+
+var str1 = "This is a string";
+var str2 = 'This is a string';
+
+console.log (str1);
+console.log (str2);
+console.log (str1 + str2);
+
+// Objects
+var marks ={
+    Subhan: 90,
+    Shafeeq: 80,
+    Zain: 70
+}
+console.log (marks);
+
+// Undefined
+
+var und = undefined;
+console.log(und);
+
+// Null
+var nul = null;
+console.log(null);
+
+// Arrays
+var arr = [1,2,3,4,5];
+console.log(arr);
